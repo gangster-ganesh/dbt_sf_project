42a82535176d2a2c5b7cccd262f4bdc8be69429a
Saiganesh <neigudl8151@gmail.com>
2025-09-21 19:14:33 +0530

    fourth commit

--- dbt_sf_pipeline/models/marts/int_order_items.sql
+++ dbt_sf_pipeline/models/marts/int_order_items.sql
@@ -3,8 +3,10 @@ select
   line_item.part_key,
   line_item.line_number,
   orders.order_key,
+  line_item.extended_price,
   orders.customer_key,
-  orders.order_date
+  orders.order_date,
+  {{discounted_amount('line_item.extended_price','line_item.discount_percentage') }} as item_discount
  from 
    {{ ref('stg_tpch_orders')}} as orders
 join 
